5bcd6a0793e1055cdfa25bd8ed19faa68a7f315a
Pauline Balangue <paulineashley.balangue@ust.edu.ph>
2026-09-02 22:31:23 +0800

    feat: initialize empty list

--- material.py
+++ material.py
@@ -1,2 +1,11 @@
 # Part 3: Data Structures - Stress and Strain Calculator Template
 # TODO: Complete this template by implementing data structures
+
+def main():
+    """Main function for the stress and strain calculator with data structures."""
+
+    print("=== Stress and Strain Calculator - Session Manager ===")
+    print()
+
+     # TODO: Initialize empty list for calculation history
+    calculation_history = []
