40c56017ff9c1d50419dbb16336311307d472559
Meg <meghanisabelle.espiritu@ust.edu.com>
2026-09-04 00:07:45 +0800

    feat: create base material class

--- properties.py
+++ properties.py
@@ -10,3 +10,11 @@ class MaterialProperties:
     def __post_init__(self):
         if self.density <= 0 or self.yield_strength <= 0 or self.typical_youngs_modulus <= 0:
             raise ValueError("All material properties must be positive values.")
+
+class Material:
+    def __init__(self, name: str, properties: MaterialProperties):
+        self.name = name
+        self.properties = properties
+    def can_withstand_stress(self, stress_mpa: float) -> bool:
+        """Check if the material can withstand the given stress (in MPa)."""
+        return stress_mpa < self.properties.yield_strength
