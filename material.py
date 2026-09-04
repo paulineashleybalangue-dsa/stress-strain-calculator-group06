d6da3ad93319c2173f5a8b31a47987dcb25bed0a
Pauline Balangue <paulineashley.balangue@ust.edu.ph>
2026-09-04 15:01:53 +0800

    feat: add Metal subclass

--- material.py
+++ material.py
@@ -6,3 +6,14 @@ class Material:
 
     def can_withstand_stress(self, stress_pa: float) -> bool:
         return stress_pa < self.properties.yield_strength
+
+class Metal(Material):
+
+    def __init__(
+        self,
+        name: str,
+        properties: MaterialProperties,
+        is_ferrous: bool = False,
+    ):
+        super().__init__(name, properties)
+        self.is_ferrous = is_ferrous
