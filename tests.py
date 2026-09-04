af91ef9e7a5bfe13226152ef7214b60bb8cdef1f
Banjo Alano <alanobanjo@gmail.com>
2026-09-04 15:09:00 +0800

    feat: create StressStrainTest class initialization

--- tests.py
+++ tests.py
@@ -8,3 +8,20 @@ from utils import (
     calculate_strain,
     calculate_stress,
 )
+
+class StressStrainTest:
+
+    def __init__(
+        self,
+        material: Material,
+        force: float,
+        area: float,
+        original_length: float,
+        change_in_length: float,
+    ):
+        self.material = material
+        self.force = force
+        self.area = area
+        self.original_length = original_length
+        self.change_in_length = change_in_length
+        self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
